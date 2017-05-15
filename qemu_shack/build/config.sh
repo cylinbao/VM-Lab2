5cecfd34163eeb853954414605a38f13c29a0239
Chien Yu Lin <myislin@gmail.com>
2017-05-15 12:32:51 +0800

    build: add another config setting for executing

--- qemu_shack/build/config.sh
+++ qemu_shack/build/config.sh
@@ -1,1 +1,2 @@
-../configure --target-list=i386-linux-user --disable-docs --enable-debug --enable-debug-tcg
+#../configure --target-list=i386-linux-user --disable-docs --enable-debug --enable-debug-tcg
+../configure --target-list=i386-linux-user --disable-docs 
